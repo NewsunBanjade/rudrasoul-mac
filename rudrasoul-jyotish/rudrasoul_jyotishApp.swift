ce73112b26d840036c87e2cb938ff1a7515ae104
Newsun <thenewsunbanjade@gmail.com>
2026-09-17 16:06:28 +0545

    Initial Commit

---
+++ rudrasoul-jyotish/rudrasoul_jyotishApp.swift
@@ -0,0 +1,17 @@
+//
+//  rudrasoul_jyotishApp.swift
+//  rudrasoul-jyotish
+//
+//  Created by Newsun on 9/17/26.
+//
+
+import SwiftUI
+
+@main
+struct rudrasoul_jyotishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ rudrasoul-jyotishTests/rudrasoul_jyotishTests.swift
@@ -0,0 +1,17 @@
+//
+//  rudrasoul_jyotishTests.swift
+//  rudrasoul-jyotishTests
+//
+//  Created by Newsun on 9/17/26.
+//
+
+import Testing
+@testable import rudrasoul_jyotish
+
+struct rudrasoul_jyotishTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
